55775a754d529e698c586b84c6f75b3b19491335
Caio Doneham <caiodonegadsg@gmail.com>
2025-03-04 13:13:55 -0300

    projeto inicial

---
+++ app.js
@@ -0,0 +1,29 @@
+let numeroSecreto = gerarNumeroAleatorio();
+
+function exibirTextoNaTela(tag, texto){
+    let campo = document.querySelector(tag, texto);
+    campo.innerHTML = texto;
+}
+exibirTextoNaTela ('h1','Hora do desafio');
+exibirTextoNaTela ('p','Escolha um número entre 1 e 10');
+
+function contagemChute() {
+    let chute = document.querySelector('input').value;
+    console.log(chute == numeroSecreto);
+}
+function exibirAlerta(){
+    alert('Eu amo JS');
+}
+function promptButton(){
+    let cidade = prompt('Qual o nome de uma cidade');
+    alert(`Estive em ${cidade} e lembrei de você`);
+}
+function somabutton(){
+    let numero1 = Number(prompt('Digite um numero'));
+    let numero2 = Number(prompt('Digite outro numero eeu irei somar'));
+    let soma = numero1 + numero2;
+    alert(`a soma dos numero digitados é ${soma}`);
+}
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1);
+}
